e57919ce07765472f143e9ad389d71ff436b81ee
sharipov23 <diagooo13@gmail.com>
2024-12-02 16:30:18 +0500

    Create ex1.3.s

---
+++ ex1.3.s
@@ -0,0 +1,5 @@
+    .global _start
+_start:
+    MOV R0, #5            @ Load first value (5)
+    MOV R1, #10           @ Load second value (10)
+    ADD R2, R0, R1        @ Add R0 and R1, store result in R2
